edb2db95dcc6ce63aa55107c187eda880a4b1077
yulia pivnenko <julia131289@gmail.com>
2022-09-16 22:57:51 +0300

    added a ball functionality

---
+++ ball.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_top_or_down(self):
+        self.y_move *= -1
+
+    def bounce_from_paddle(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_from_paddle()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 # screen setup
 screen = Screen()
@@ -7,6 +9,9 @@ screen.bgcolor("black")
 screen.setup(width=700, height=500)
 screen.title("Ping Pong")
 screen.tracer(0)
+
+# setup ball
+ball = Ball()
 
 # setup paddle
 paddle_right = Paddle(330, 0)
@@ -22,6 +27,23 @@ screen.onkey(paddle_left.down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    # Detect collision with wall
+    if ball.ycor() > 220 or ball.ycor() < -220:
+        ball.bounce_top_or_down()
+
+    # Detect collision with paddle
+    if ball.distance(paddle_right) < 30 and ball.xcor() > 280 or ball.distance(paddle_left) < 30 and ball.xcor() < -280:
+        ball.bounce_from_paddle()
+
+    # Detect right paddle misses
+    if ball.xcor() > 340:
+        ball.reset_position()
+
+    # Detect left paddle misses
+    if ball.xcor() < -340:
+        ball.reset_position()
 
 screen.exitonclick()
